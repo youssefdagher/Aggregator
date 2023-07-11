b09a2798a9f715ff7f0deb0e11e16de146693d4f
Elia <Elia@DESKTOP-HJEUEDP>
2023-07-11 14:40:33 +0300

    Add project files.

---
+++ Aggregator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aggregator
+{
+    public static class Aggregator
+    {
+        public static string ConnectionString { get; private set; } = ConfigurationManager.ConnectionStrings["FYPConnectionString"].ConnectionString;
+        public static void CallRAN2G15MinAggSP()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand("RAN_2G_15MIN_AGG_SP", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static void CallRAN2GCountersSP()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand("RAN_2G_COUNTERS_SP", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static void CallRAN2GKPIsSP()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand("RAN_2G_KPIS_SP", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aggregator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        { 
+            Aggregator.CallRAN2G15MinAggSP();
+            Aggregator.CallRAN2GCountersSP();
+            Aggregator.CallRAN2GKPIsSP();
+        }
+    }
+}
